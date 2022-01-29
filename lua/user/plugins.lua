3ec9a38108ba221d6b2179207219e0d52ae691c9
Nishit Gupta <nishit.gupta6708@gmail.com>
2022-01-29 22:32:09 +0530

    refactor: Bufferline

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -73,6 +73,13 @@ return packer.startup(function(use)
 
   -- NvimTree
   use "kyazdani42/nvim-tree.lua"
+
+  -- Bufferline
+  use "akinsho/bufferline.nvim"
+  
+
+
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
