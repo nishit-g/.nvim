d72404b929ff6037ea796ed8c4b162a3afabe4ce
Nishit Gupta <nishit.gupta6708@gmail.com>
2022-01-29 21:52:46 +0530

    refactor: gitsigns

--- after-BAK/plugin/autopairs.lua
+++ after-BAK/plugin/autopairs.lua
@@ -2,4 +2,4 @@ local status, formatter = pcall(require, "nvim-autopairs")
 if (not status) then return end
 
 
-require('nvim-autopairs').setup{}
+formatter.setup{}
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -109,6 +109,11 @@ _G.packer_plugins = {
     path = "/Users/nishit.gupta/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
+  ["gitsigns.nvim"] = {
+    loaded = true,
+    path = "/Users/nishit.gupta/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
+    url = "https://github.com/lewis6991/gitsigns.nvim"
+  },
   ["gruvbox-material"] = {
     loaded = true,
     path = "/Users/nishit.gupta/.local/share/nvim/site/pack/packer/start/gruvbox-material",
