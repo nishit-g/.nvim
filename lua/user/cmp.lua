f0b8cc37928c41ccd7ce0149f01322f2e308de58
Nishit Gupta <nishit.gupta6708@gmail.com>
2022-01-29 22:40:40 +0530

    clean files

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -15,34 +15,32 @@ local check_backspace = function()
   return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
 end
 
--- ? ? ?? ? ? ? some other good icons
--- TO BE DONE LATER
 local kind_icons = {
-  Text = "?",
+  Text = "",
   Method = "m",
-  Function = "?",
-  Constructor = "?",
-  Field = "?",
-  Variable = "?",
-  Class = "?",
-  Interface = "?",
-  Module = "?",
-  Property = "?",
-  Unit = "?",
-  Value = "?",
-  Enum = "?",
-  Keyword = "?",
-  Snippet = "?",
-  Color = "?",
-  File = "?",
-  Reference = "?",
-  Folder = "?",
-  EnumMember = "?",
-  Constant = "?",
-  Struct = "?",
-  Event = "?",
-  Operator = "?",
-  TypeParameter = "?",
+  Function = "",
+  Constructor = "",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "",
+  Property = "",
+  Unit = "",
+  Value = "",
+  Enum = "",
+  Keyword = "",
+  Snippet = "",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "",
+  EnumMember = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
 }
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
@@ -97,7 +95,7 @@ cmp.setup {
     fields = { "abbr", "menu", "kind" },
     format = function(entry, vim_item)
       -- Kind icons
-      -- vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
@@ -117,6 +115,9 @@ cmp.setup {
   confirm_opts = {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
+  },
+  documentation = {
+    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
   },
   experimental = {
     ghost_text = false,
