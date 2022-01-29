7578a720e6e28c78c8ca63ad4b757b55652bcddb
Nishit Gupta <nishit.gupta6708@gmail.com>
2022-01-29 15:04:32 +0530

    refactor: options

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -32,7 +32,7 @@ end
 -- Install your plugins here
 return packer.startup(function(use)
   -- My plugins here
-
+  use "wbthomason/packer.nvim" -- Have packer manage itself
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
