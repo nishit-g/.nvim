9f7d6b1b3231a6f0f3aeea9b2c24a166cef9d437
Nishit Gupta <nishit.gupta6708@gmail.com>
2022-06-20 14:49:26 +0530

    feat: jdtls

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -51,6 +51,8 @@ cmp.setup({
 		end,
 	},
 	mapping = {
+		["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+		["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
 		["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
 		["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
 		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
