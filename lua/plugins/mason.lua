f0033d929d3574ddf09d5fd64e3920d3fa229ec9
Nishit Gupta <nishit.gupta6708@gmail.com>
2023-11-17 22:48:47 +0530

    feat: setup lspconfig + mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,9 +1,22 @@
 return {
     "williamboman/mason.nvim",
-    cmd = {"Mason", "MasonUpdate", "MasonInstall", "MasonLog"},
+    lazy = false,
+    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
+    build = ":MasonUpdate",
+    dependencies = {
+      {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+      },
+    },
     config = function()
       local status_ok, mason = pcall(require, "mason")
       if not status_ok then
+        return
+      end
+
+      local status_ok_lsp, mason_lspconfig = pcall(require, "mason-lspconfig")
+      if not status_ok_lsp then
         return
       end
 
@@ -16,5 +29,7 @@ return {
               }
           }
       }
+
+      mason_lspconfig.setup{}
     end,
 }
