273830c198d64b390ec0823d75249916e29789e5
Nishit Gupta <nishit.gupta6708@gmail.com>
2023-11-17 21:55:31 +0530

    feat: lazy load plugins

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,7 +1,9 @@
 -- Best Git plugin alskdfjal
 return {
   "tpope/vim-fugitive",
+  opt = true,  -- Marks the plugin for lazy loading
+  cmd = { 'G', 'Git', 'Gstatus', 'Gblame', 'Gdiff' },
   dependencies = {
     "tpope/vim-rhubarb"
-  }
+  },
 }
