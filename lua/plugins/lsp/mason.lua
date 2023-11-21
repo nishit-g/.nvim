80467e356030fae9efef902f7ac8341a5fd3b5ef
Nishit Gupta <nishit.gupta6708@gmail.com>
2023-11-21 16:17:40 +0530

    feat: add debugging for js, ts

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,35 +1,42 @@
 return {
-    "williamboman/mason.nvim",
-    lazy = false,
-    cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
-    build = ":MasonUpdate",
-    dependencies = {
-      {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-      },
-    },
-    config = function()
-      local status_ok, mason = pcall(require, "mason")
-      if not status_ok then
-        return
-      end
+	"williamboman/mason.nvim",
+	lazy = false,
+	cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
+	build = ":MasonUpdate",
+	opts = {
+		ensure_installed = {
+			"eslint-lsp",
+			"prettier",
+			"typescript-language-server",
+		},
+	},
+	dependencies = {
+		{
+			"williamboman/mason-lspconfig.nvim",
+			lazy = false,
+		},
+	},
+	config = function()
+		local status_ok, mason = pcall(require, "mason")
+		if not status_ok then
+			return
+		end
 
-      local status_ok_lsp, mason_lspconfig = pcall(require, "mason-lspconfig")
-      if not status_ok_lsp then
-        return
-      end
+		local status_ok_lsp, mason_lspconfig = pcall(require, "mason-lspconfig")
+		if not status_ok_lsp then
+			return
+		end
 
-      mason.setup{
-          ui = {
-              icons = {
-                  package_installed = "✓",
-                  package_pending = "➜",
-                  package_uninstalled = "✗"
-              }
-          }
-      }
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-      mason_lspconfig.setup{}
-    end,
+		mason_lspconfig.setup({})
+	end,
 }
