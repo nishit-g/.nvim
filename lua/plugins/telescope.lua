acbd2cf63795aca58016ae727a3ccf9be10cce33
Nishit Gupta <nishit.gupta6708@gmail.com>
2024-08-29 18:46:53 +0000

    update: plugin config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,93 +1,109 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.4',
-    cmd = "Telescope",
-    dependencies = {
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-      },
-      {'nvim-lua/plenary.nvim'}
-    },
-    config = function () 
+	"nvim-telescope/telescope.nvim",
+	cmd = "Telescope",
+	version = false,
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
+		{ "nvim-tree/nvim-web-devicons" },
+		{ "nvim-telescope/telescope-ui-select.nvim" },
+		{ "nvim-telescope/telescope-file-browser.nvim" },
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
 
-      local telescope_status_ok, telescope = pcall(require, "telescope")
-      if not telescope_status_ok then
-          return
-      end
+		telescope.setup({
+			defaults = {
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+				},
+				prompt_prefix = " ",
+				selection_caret = " ",
+				entry_prefix = "  ",
+				initial_mode = "insert",
+				selection_strategy = "reset",
+				sorting_strategy = "ascending",
+				layout_strategy = "horizontal",
+				layout_config = {
+					horizontal = {
+						prompt_position = "top",
+						preview_width = 0.55,
+						results_width = 0.8,
+					},
+					vertical = {
+						mirror = false,
+					},
+					width = 0.87,
+					height = 0.80,
+					preview_cutoff = 120,
+				},
+				file_sorter = require("telescope.sorters").get_fuzzy_file,
+				file_ignore_patterns = { "%.git/", "node_modules" },
+				generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+				path_display = { "truncate" },
+				winblend = 0,
+				border = {},
+				borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+				color_devicons = true,
+				set_env = { ["COLORTERM"] = "truecolor" },
+				file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+				grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+				qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+				buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+			},
+			pickers = {
+				find_files = {
+					hidden = true,
+					no_ignore = false,
+					find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
+				},
+			},
+			extensions = {
+				fzf = {
+					fuzzy = true,
+					override_generic_sorter = true,
+					override_file_sorter = true,
+					case_mode = "smart_case",
+				},
+				file_browser = {
+					theme = "dropdown",
+					hijack_netrw = true,
+					mappings = {
+						["i"] = {
+							["<C-w>"] = function()
+								vim.cmd("normal vbd")
+							end,
+						},
+						["n"] = {
+							["N"] = require("telescope").extensions.file_browser.actions.create,
+							["h"] = require("telescope").extensions.file_browser.actions.goto_parent_dir,
+						},
+					},
+				},
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown({
+						-- even more opts
+					}),
+				},
+			},
+		})
 
-      local telescope_actions_status_ok, actions = pcall(require, "telescope.actions")
-      if not telescope_actions_status_ok then
-          return
-      end
-
-      local extensions = {
-          fzf = {
-            fuzzy = true, -- false will only do exact matching
-            override_generic_sorter = false, -- override the generic sorter
-            override_file_sorter = true, -- override the file sorter
-            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-            -- the default case_mode is "smart_case"
-          },
-      }
-
-      local pickers = {
-        oldfiles = {
-          prompt_title = "Recent Files",
-        },
-      }
-
-      telescope.setup {
-        defaults = {
-          vimgrep_arguments = {
-            "rg",
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--smart-case",
-          },
-          mappings = {
-            n = {
-              ["q"] = actions.close,
-            },
-          },
-          prompt_prefix = " ",
-          selection_caret = " ",
-          entry_prefix = "  ",
-          initial_mode = "insert",
-          selection_strategy = "reset",
-          sorting_strategy = "ascending",
-          layout_strategy = "horizontal",
-          layout_config = {
-            horizontal = {
-              prompt_position = "bottom",
-              preview_width = 0.55,
-              results_width = 0.8,
-            },
-            vertical = {
-              mirror = false,
-            },
-            width = 0.87,
-            height = 0.80,
-            preview_cutoff = 120,
-          },
-          file_sorter = require("telescope.sorters").get_fuzzy_file,
-          file_ignore_patterns = { ".git/",".git\\", "node_modules" },          
-          path_display = { "absolute" },
-          winblend = 0,
-          border = true,
-          borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-          color_devicons = true,
-          use_less = true,
-          generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-          set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-        },
-        extensions = extensions,
-        pickers = pickers
-      }
-
-      telescope.load_extension "fzf"
-
-    end
+		-- Load extensions
+		pcall(telescope.load_extension, "fzf")
+		pcall(telescope.load_extension, "ui-select")
+		pcall(telescope.load_extension, "file_browser")
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,27 +1,33 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	lazy = false,
+	event = { "BufReadPost", "BufNewFile" },
 	config = function()
-		local configs = require("nvim-treesitter.configs")
-
-		configs.setup({
+		require("nvim-treesitter.configs").setup({
 			ensure_installed = {
 				"lua",
 				"javascript",
-				"html",
 				"typescript",
+				"html",
+				"css",
+				"json",
+				"yaml",
 			},
 			sync_install = false,
+			auto_install = true,
 			highlight = {
 				enable = true,
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
+				disable = { "" },
 				additional_vim_regex_highlighting = false,
 			},
-			-- indent = { enable = true },
+			indent = {
+				enable = true,
+				disable = { "yaml" },
+			},
+			context_commentstring = {
+				enable = true,
+				enable_autocmd = false,
+			},
 		})
 	end,
 }
